0b61b1992f89d625db478f468b0b98cd75229bb4
Guoqi Sun <sun412926@gmail.com>
2024-11-18 00:54:14 +0800

    chore: configure husky, lint-stage, prettier and eslint

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,10 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
-export default {
+const config = {
+  semi: false,
+  singleQuote: false,
+  trailingComma: "all",
+  endOfLine: "lf",
   plugins: ["prettier-plugin-tailwindcss"],
 };
+
+export default config;
